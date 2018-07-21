b34d310293f63f8dc86327f95431b9778390892c
Jonathan  Foster <jfoster91@192.168.1.108>
2018-07-21 11:17:15 +0100

    lesson3

--- app/src/main/java/com/example/jfoster91/swooosh/WelconeActivity.kt
+++ app/src/main/java/com/example/jfoster91/swooosh/WelconeActivity.kt
@@ -1,12 +1,19 @@
 package com.example.jfoster91.swooosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcone.*
 
 class WelconeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcone)
+
+        getStartedBtn.setOnClickListener {
+            val leagueIntent = Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
     }
 }
