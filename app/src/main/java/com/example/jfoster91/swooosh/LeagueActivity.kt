b285a7f8fc759b3f11483aeb36106f3cf0b1e608
Jonathan  Foster <jfoster91@192.168.1.108>
2018-07-21 13:16:04 +0100

    lesson 6

--- app/src/main/java/com/example/jfoster91/swooosh/LeagueActivity.kt
+++ app/src/main/java/com/example/jfoster91/swooosh/LeagueActivity.kt
@@ -1,12 +1,25 @@
 package com.example.jfoster91.swooosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_league.*
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+
+        }
+
+
+    fun leagueNextClicked(view: View){
+        val skillIntent = Intent(this, SkillActivity::class.java)
+        startActivity(skillIntent)
     }
+
+
 }
+
--- app/src/main/java/com/example/jfoster91/swooosh/WelconeActivity.kt
+++ app/src/main/java/com/example/jfoster91/swooosh/WelconeActivity.kt
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_welcone.*
 
-class WelconeActivity : AppCompatActivity() {
+class WelconeActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
